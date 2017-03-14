6464976f4e5a28176f6b8fa9a9f421f8f5c6b73a
cathyob <cathy.obrien09@gmail.com>
2017-03-14 19:17:06 -0400

    Ruby Practice - Calculator Partial Response
    was able to complete 5 tests within timebox

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 def calc(expr)
-  # Your code here
+  return 0 if expr.empty?
+
+  split = expr.split(' ')
+
+  var1 = split[0].to_i
+  var2 = split[1].to_i
+
+  case split.last
+  when '+' then var1 + var2
+  when '-' then var1 - var2
+  when '*' then var1 * var2
+  when '/' then var1 / var2
+  end
 end
